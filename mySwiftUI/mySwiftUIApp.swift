96916e31f90ce13158a03247ece289ea8e878ddb
flyingking888 <jeremyfeiwang@outlook.com>
2025-04-16 14:32:39 -0400

    Initial Commit

---
+++ mySwiftUI/mySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySwiftUIApp.swift
+//  mySwiftUI
+//
+//  Created by Jeremy Wang on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct mySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
