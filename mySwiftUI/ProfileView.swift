2b379321e87f1868a3340b43fd2ed6892f56be84
flyingking888 <jeremyfeiwang@outlook.com>
2025-04-21 13:39:51 -0400

    4-21-2025

--- mySwiftUI/ProfileView.swift
+++ mySwiftUI/ProfileView.swift
@@ -7,18 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ProfileView: View {
+    //Declare some properties
+    
+    @State private var newComment: String = ""
+    @State private var Comments: [String] = []
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(UIColor(red: 0.75, green: 0.22, blue: 0.17, alpha: 1))
+                .ignoresSafeArea()
+            
+            VStack(spacing: 16){
+                Image("Jeremy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 6))
+                    .shadow(radius: 8)
+                
+                Text("Hi Flagler Student")
+                    .font(.title2)
+                    .bold()
+                    .italic()
+                    .underline()
+                    .foregroundColor(.white)
+                
+                HStack{
+                    Text("Comment: 💬")
+                        .foregroundColor(.white)
+                    
+                    TextField("Add your comments here", text: $newComment)
+                        .frame(height: 40)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(12)
+                }
+                .padding()
+                
+                
+                
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ProfileView()
 }
+
--- mySwiftUI/mySwiftUIApp.swift
+++ mySwiftUI/mySwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct mySwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProfileView()
         }
     }
 }
